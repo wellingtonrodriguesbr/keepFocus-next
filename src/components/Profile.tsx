78c54e6f937253bf0d4607cc54596a2e9bee4299
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2021-12-04 14:51:59 -0300

    initial commit new project

---
+++ src/components/Profile.tsx
@@ -0,0 +1,19 @@
+import styles from "../styles/components/Profile.module.css";
+
+export function Profile() {
+  return (
+    <div className={styles.profileContainer}>
+      <img
+        src="https://github.com/wellingtonrodriguesbr.png"
+        alt="Imagem de perfil do usuário"
+      />
+      <div>
+        <strong>Wellington Rodrigues</strong>
+        <p>
+          <img src="icons/level.svg" alt="Seta verde apontando para cima" />
+          Level 1
+        </p>
+      </div>
+    </div>
+  );
+}
