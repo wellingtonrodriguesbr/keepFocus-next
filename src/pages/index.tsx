1414d1e9c5161c63797db591b656fcd850a9c431
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2021-12-05 12:09:40 -0300

    added component Countdown and Hooks

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { CompletedChallenges } from "../components/CompletedChallenges";
+import { Countdown } from "../components/Countdown";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
 import styles from "../styles/pages/Home.module.css";
@@ -11,6 +12,7 @@ export default function Home() {
         <div>
           <Profile />
           <CompletedChallenges />
+          <Countdown />
         </div>
         <div></div>
       </section>
