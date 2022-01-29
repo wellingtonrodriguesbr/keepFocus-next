07b5261dd300b2a71868a637fda2d129ad218dc9
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2022-01-29 10:56:35 -0300

    fix: disable notification

--- src/context/ChallengesContext.tsx
+++ src/context/ChallengesContext.tsx
@@ -46,9 +46,9 @@ export function ChallengesProvider({
 
   const experienceToNextLevel = Math.pow((level + 1) * 4, 2);
 
-  useEffect(() => {
-    Notification.requestPermission();
-  }, []);
+  // useEffect(() => {
+  //   Notification.requestPermission();
+  // }, []);
 
   useEffect(() => {
     Cookies.set("level", String(level));
@@ -73,11 +73,11 @@ export function ChallengesProvider({
 
     new Audio("/notification.mp3").play();
 
-    if (Notification.permission === "granted") {
-      new Notification("Novo desafio Keep Focus 🏆", {
-        body: `Valendo ${challenge.amount} xp`,
-      });
-    }
+    // if (Notification.permission === "granted") {
+    //   new Notification("Novo desafio Keep Focus 🏆", {
+    //     body: `Valendo ${challenge.amount} xp`,
+    //   });
+    // }
   }
 
   function resetChallenge() {
